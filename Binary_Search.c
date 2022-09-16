b3465660205a36fdb2f4f9e258b155ee37ffa378
hack <uttampipliya4@gmail.com>
2022-09-16 20:46:01 +0530

    Search Algorithm

--- Binary_Search.c
+++ Binary_Search.c
@@ -6,7 +6,7 @@ int binarySearch(int arr[], int l, int r, int x)
 		int mid = l + (r - l) / 2;
 		if (arr[mid] == x)
 			return mid;
-		if (arr[mid] > x)
+		if (arr[mid] < x)
 			return binarySearch(arr, l, mid - 1, x);
 		return binarySearch(arr, mid + 1, r, x);
 	}
