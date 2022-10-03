3d6a7847f29769ef63ec593da47146d4fdd014ab
Uttam patel <utampipaliya@gmail.com>
2022-10-03 22:58:10 +0530

    update

--- Bubble_sort.c
+++ Bubble_sort.c
@@ -26,3 +26,31 @@ int main() {
   printf("Sorted Array :\n");
   printArray(data, size);
 }
+
+// n = 5
+// arr = 5 3 1 6 4
+
+//     pase 1:
+//         i = 0 
+//         - 3 5 1 6 4          n = 5
+//         i = 1               i stop = 3
+//         - 3 1 5 6 4
+//         i = 2
+//         - 3 1 5 6 4
+//         i = 3
+//         - 3 1 5 4 6
+
+
+//     pase 2:
+//         i = 0 
+//         - 1 3 5 4 6          i stop = 2
+//         i = 1
+//         - 1 3 5 4 6
+//         i = 2
+//         - 1 3 4 5 6
+
+//     pase 3:
+//         i = 0 
+//         - 1 3 4 5 6
+//                                flag = 1
+//                                then stop 
